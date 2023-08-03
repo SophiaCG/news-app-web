cba27b64250a350c162c019478d48825971c8302
SophiaCG <sophia.gorgonio@gmail.com>
2023-08-03 18:15:09 -0400

    Adjusted sizing for different screen sizes

--- src/components/NewsRowGroup.js
+++ src/components/NewsRowGroup.js
@@ -4,13 +4,13 @@ import NewsRow from "./NewsRow";
 const NewsRowGroup = ({ data }) => {
   const renderRows = () => {
     const rows = [];
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i < 5; i++) {
       rows.push(<NewsRow props={data.articles[i]}></NewsRow>);
     }
     return rows;
   };
 
-  return <div>{renderRows()}</div>;
+  return <div id="news-row-group">{renderRows()}</div>;
 };
 
 export default NewsRowGroup;
